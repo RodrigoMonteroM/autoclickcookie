67b151a8ac7dd1d27d0b7d7386248e7682783068
rodrigo montero <rodrigomontero240303@gmail.com>
2023-08-31 22:08:46 +0200

    added headless true

--- disc/index.js
+++ disc/index.js
@@ -2,9 +2,7 @@ import { chromium } from "playwright";
 import fs from "fs";
 (async () => {
     // Setup
-    const browser = await chromium.launch({
-        headless: false,
-    });
+    const browser = await chromium.launch({ headless: true });
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://orteil.dashnet.org/cookieclicker/");
