93b19b429ef181960fb19a76be3da3d6f9f8c1d6
rodrigo montero <rodrigomontero240303@gmail.com>
2023-08-31 21:48:39 +0200

    add headless

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,7 @@ import fs from "fs";
 
 (async () => {
   // Setup
-  const browser = await chromium.launch({
-    headless: true,
-  });
+  const browser = await chromium.launch();
   const context = await browser.newContext();
 
   const page = await context.newPage();
